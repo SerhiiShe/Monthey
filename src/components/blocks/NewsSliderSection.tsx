49643d28b69427f24455bd354b339b506dd29d28
SerhiiShe <shevchenko200494@gmail.com>
2026-08-08 15:07:22 +0300

    News draft mode

--- src/components/blocks/NewsSliderSection.tsx
+++ src/components/blocks/NewsSliderSection.tsx
@@ -2,15 +2,21 @@ import configPromise from '@payload-config'
 import { NewsSliderSectionType } from '@/payload-types'
 import NewsSliderSectionClient from '../NewsSliderSectionClient'
 import { getPayload } from 'payload'
+import { draftMode } from 'next/headers'
 
 export default async function NewsSliderSection(props: NewsSliderSectionType) {
+  const draft = await draftMode()
+  const isDraftMode = draft.isEnabled
+
   const payload = await getPayload({ config: configPromise })
   const newsData = await payload.find({
     collection: 'news',
     sort: '-publishedDate',
+    draft: isDraftMode,
   })
   const newsCategoriesData = await payload.find({
     collection: 'news-categories',
+    draft: isDraftMode,
   })
 
   return (
